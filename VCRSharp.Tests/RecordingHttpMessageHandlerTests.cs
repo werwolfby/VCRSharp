612b57096eb3beacdeb2af19ca80c9657247c04d
Alexander Puzynia <werwolf.by@gmail.com>
2019-11-25 22:10:11 -0800

    test: additional test coverage for RecordingHttpMessageHandlerTests

--- VCRSharp.Tests/RecordingHttpMessageHandlerTests.cs
+++ VCRSharp.Tests/RecordingHttpMessageHandlerTests.cs
@@ -33,6 +33,31 @@ namespace VCRSharp.Tests
             Assert.That(cassette.Records[0].Response.Headers["Server"], Is.EqualTo("Test-Server"));
         }
         
+        [Test]
+        public async Task SendAsync_PostRequest_Success()
+        {
+            var cassette = new Cassette();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("http://localhost:8080"),
+                Content = new StringContent("{}"),
+            };
+
+            var handler = new PublicRecordingHttpMessageHandler(new MockHttpRequestHandler(), cassette);
+            await handler.SendAsync(request, CancellationToken.None);
+            
+            Assert.That(cassette.Records, Has.Count.EqualTo(1));
+            Assert.That(cassette.Records[0].Request.Method, Is.EqualTo(request.Method.Method));
+            Assert.That(cassette.Records[0].Request.Uri, Is.EqualTo(request.RequestUri));
+            Assert.That(cassette.Records[0].Request.Body, Is.EqualTo("{}"));
+            
+            Assert.That(cassette.Records[0].Response.Version, Is.EqualTo(new Version(1, 1)));
+            Assert.That(cassette.Records[0].Response.StatusCode, Is.EqualTo(200));
+            Assert.That(cassette.Records[0].Response.Headers, Has.Count.EqualTo(1));
+            Assert.That(cassette.Records[0].Response.Headers["Server"], Is.EqualTo("Test-Server"));
+        }
+        
         private class PublicRecordingHttpMessageHandler : RecordingHttpMessageHandler
         {
             public PublicRecordingHttpMessageHandler(HttpMessageHandler innerHandler, Cassette cassette) : base(innerHandler, cassette)
