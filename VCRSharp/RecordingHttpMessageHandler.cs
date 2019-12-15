acc9f1089f340cc1f6f283b83fd9bdf38d0cc4da
Alexander Puzynia <werwolf.by@gmail.com>
2019-12-15 00:54:21 -0800

    fix: for compare request and response request use cloned cookie container

--- VCRSharp/RecordingHttpMessageHandler.cs
+++ VCRSharp/RecordingHttpMessageHandler.cs
@@ -22,10 +22,14 @@ namespace VCRSharp
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var recordRequest = await CassetteRecordRequest.CreateFromRequest(request, _cookieContainer);
+            
+            // After sending request _cookieContainer can be extended and recordRequest will definitely change as well
+            // cause Cookie header will be added, that's why for compare we should clone CookieContainer
+            var cloneCookieContainer = _cookieContainer?.Clone(request.RequestUri);
 
             var response = await base.SendAsync(request, cancellationToken);
 
-            var recordResponse = await CassetteRecordResponse.CreateFromResponse(response, recordRequest, _cookieContainer);
+            var recordResponse = await CassetteRecordResponse.CreateFromResponse(response, recordRequest, cloneCookieContainer);
             
             var record = new CassetteRecord(recordRequest, recordResponse);
             _cassette.Add(record);
