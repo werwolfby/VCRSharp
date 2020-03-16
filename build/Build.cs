b4bc130ffd7fc52d9bd852a809a666dd195e4a30
Alexander Puzynia <werwolf.by@gmail.com>
2020-03-16 00:17:25 -0700

    test: add coverage and generate coverage report

--- build/Build.cs
+++ build/Build.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using Microsoft.VisualBasic;
 using Nuke.Common;
 using Nuke.Common.Execution;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.ReportGenerator;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.Tools.NUnit.NUnitTasks;
+using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
@@ -27,15 +32,27 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Parameter("Specify to not open generated report")]
+    readonly bool NotOpenReport = false;
+
     [Solution] readonly Solution Solution;
     Project UnitTestsProject => Solution.GetProject("VCRSharp.Tests");
+    
     Project IntegrationTestsProject => Solution.GetProject("VCRSharp.IntegrationTests");
+
+    AbsolutePath ReportsDirectory => RootDirectory / "Reports";
+    
+    List<AbsolutePath> CoverageReports { get; } = new List<AbsolutePath>();
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
-            foreach (var buildDir in GlobDirectories(RootDirectory, "**/bin").Concat(GlobDirectories(RootDirectory, "**/obj")).Where(d => !d.Contains("build")))
+            foreach (var buildDir in GlobDirectories(RootDirectory, "**/bin")
+                .Concat(GlobDirectories(RootDirectory, "**/obj"))
+                .Concat(GlobDirectories(RootDirectory, "*Tests/TestResults"))
+                .Concat(ReportsDirectory)
+                .Where(d => !d.Contains("build")))
             {
                 EnsureCleanDirectory(buildDir);
             }
@@ -66,8 +83,10 @@ class Build : NukeBuild
         .DependsOn(Compile)
         .Executes(() =>
         {
-            DotNetTest(s => s
-                .SetProjectFile(UnitTestsProject.Path));
+            var outputs = DotNetTest(s => s
+                .SetProjectFile(UnitTestsProject.Path)
+                .SetDataCollector("XPlat Code Coverage"));
+            CoverageReports.Add(GetAttachment(outputs));
         });
 
     Target IntegrationTests => _ => _
@@ -78,4 +97,38 @@ class Build : NukeBuild
             DotNetTest(s => s
                 .SetProjectFile(IntegrationTestsProject.Path));
         });
+
+    Target CoverageReport => _ => _
+        .After(UnitTests)
+        .After(IntegrationTests)
+        .Executes(() =>
+        {
+            ControlFlow.Assert(CoverageReports.Count > 0,
+                "CoverageReport task has to be executed with *Tests target's only");
+
+            ReportGenerator(s => s
+                .SetFramework("netcoreapp3.0")
+                .SetReports(CoverageReports.Select(s => (string)s))
+                .SetReportTypes(ReportTypes.HtmlInline_AzurePipelines)
+                .SetTargetDirectory(ReportsDirectory));
+            
+            if (NotOpenReport) return;
+
+            if (IsWin)
+            {
+                Process.Start("cmd.exe", @"/c " + ReportsDirectory / "index.htm");
+            }
+        });
+
+    AbsolutePath GetAttachment(IEnumerable<Output> outputs)
+    {
+        var attachment = outputs
+            .Where(o => o.Type == OutputType.Std)
+            .SkipWhile(o => !o.Text.StartsWith("Attachments:"))
+            .Skip(1)
+            .Cast<Output?>()
+            .FirstOrDefault();
+        ControlFlow.Assert(attachment != null, "Can't find test result");
+        return (AbsolutePath) attachment.Value.Text.Trim();
+    }
 }
